adf01d249e4100d6438ee88018909b18c529d6a6
rahatramim07 <hossainraj282@gmail.com>
2025-03-02 15:40:42 +0600

    Add files via upload

---
+++ Feb23_printf.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{   
+    //Problem 1
+    printf("Bangladesh is my country. I Love my country.\n\n");
+
+    //Problem 2
+    printf("The woods are lovely, dark and deep,\n");
+    printf("But I have promises to keep,\n");
+    printf("And miles to go before I sleep,\n");
+    printf("And miles to go before I sleep,\n");
+
+    //Problem 3
+    printf("\n1\n12\n123\n1234\n");
+
+    //Problem 4
+    printf("\n*\n**\n***\n****\n***\n**\n*\n");
+    
+    //Problem 5
+    printf("\n****** \n*    *\n*    *\n*    *\n*    *\n******\n");
+
+    //Problem 6
+    printf("\n   1  \n  121\n 12321\n1234321");
+
+    //Problem 7
+    printf("\n\n    *\n   * *\n  * * *\n * * * *\n  * * *\n   * *\n    *\n");
+
+    //Problem 8
+    printf("\n *******\n  *****\n   ***\n    *\n   ***\n  *****\n *******\n");
+
+    //Problem 9
+    int a = 11;
+    int b = 22;
+    int sum = a + b;
+    printf("\n\n%d", sum);
+
+    //Problem 10
+    int c = 10;
+    int d = 25;
+    int mul = c * d;
+    printf("\n\n%d", mul);
+
+    //Problem 11
+    int e = 6;
+    int f = 3;
+    int div = e / f;
+    printf("\n\n%d", div);
+
+    //Problem 12
+    int g = 135;
+    int h = 2;
+    int mod = g % h;
+    printf("\n\n%d", mod);
+
+
+    return 0;
+}
